b6ce43ad25e71749fbcf9581d96cf5b4c2dd3240
motemen <motemen@gmail.com>
2013-09-26 01:23:43 +0900

    core spec

---
+++ spec/lib/git/browse/remote/core_spec.rb
@@ -0,0 +1,123 @@
+require 'git/browse/remote/core'
+
+describe Git::Browse::Remote::Core do
+  subject(:core) { Git::Browse::Remote::Core.new }
+
+  describe '#template_type' do
+    context 'if @file set' do
+      before { core.file = 'README.md' }
+
+      it 'is :file' do
+        expect(core.template_type).to be(:file)
+      end
+    end
+
+    context 'if @file not set' do
+      it 'is same as #mode' do
+        expect(core.template_type).to be(core.mode)
+      end
+    end
+  end
+
+  describe '#remote' do
+    context 'by default' do
+      it 'is "origin"' do
+        expect(core.remote).to eq('origin')
+      end
+    end
+
+    context 'if remote set' do
+      before { core.remote = 'remote-foo' }
+
+      it 'is the set value' do
+        expect(core.remote).to eq('remote-foo')
+      end
+    end
+
+    context 'if #ref is a remote ref' do
+      it 'is the remote name in the ref' do
+        core.stub(:ref).and_return('remotes/remote-bar/master')
+        expect(core.remote).to eq('remote-bar')
+      end
+    end
+  end
+
+  describe '#mode' do
+    context 'by default' do
+      it 'is :rev' do
+        expect(core.mode).to eq(:rev)
+      end
+    end
+
+    context 'if mode set' do
+      before { core.mode = :foo }
+
+      it 'is the set value' do
+        expect(core.mode).to eq(:foo)
+      end
+    end
+
+    context 'if #ref points to master' do
+      it 'is :top' do
+        core.stub(:ref).and_return('refs/heads/master')
+        expect(core.mode).to eq(:top)
+      end
+    end
+
+    context 'if #ref points to some ref' do
+      it 'is :ref' do
+        core.stub(:ref).and_return('refs/heads/branch-a')
+        expect(core.mode).to eq(:ref)
+      end
+    end
+  end
+
+  describe '#commit' do
+    it 'calls Git.parse_rev with #target' do
+      Git::Browse::Remote::Git.should_receive(:parse_rev).with('__TARGET__').and_return('0000000000000000000000000000000000000000')
+      core.stub(:target).and_return('__TARGET__')
+      expect(core.commit).to eq('0000000000000000000000000000000000000000')
+    end
+  end
+
+  describe '#short_commit' do
+    it 'calls Git.parse_rev_short with #target' do
+      Git::Browse::Remote::Git.should_receive(:parse_rev_short).with('__TARGET__').and_return('00000000')
+      core.stub(:target).and_return('__TARGET__')
+      expect(core.short_commit).to eq('00000000')
+    end
+  end
+
+  describe '#rev' do
+    context 'if #mode is not :rev' do
+      before { code.stub(:mode).and_return(:rev) }
+
+      context 'and #_ref is not defined' do
+        xit 'returns the #_commit value'
+      end
+
+      context 'and #_ref is defined' do
+        xit 'returns the #_ref value'
+      end
+    end
+
+    context 'if #mode is :rev' do
+      context 'and #_ref is not defined' do
+        xit 'returns the #_commit value'
+      end
+
+      context 'and #_ref is defined' do
+        xit 'returns the #_commit value though'
+      end
+    end
+  end
+
+  describe '#ref' do
+  end
+
+  describe '#target' do
+  end
+
+  describe '#url' do
+  end
+end
